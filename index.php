26b76aabc530c9c3be539a018504f515e9355a9f
giorgioriccardi <griccardi@my.bcit.ca>
2019-02-11 14:00:06 -0800

    inject wp header and footer in main template index.php

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
 		<link rel="stylesheet" type="text/css" href="css/base.css" />
 		<script>document.documentElement.className="js";var supportsCssVars=function(){var e,t=document.createElement("style");return t.innerHTML="root: { --tmp-var: bold; }",document.head.appendChild(t),e=!!(window.CSS&&window.CSS.supports&&window.CSS.supports("font-weight","var(--tmp-var)")),t.parentNode.removeChild(t),e};supportsCssVars()||alert("Please view this demo in a modern browser that supports CSS Variables.");</script>
 	</head>
+
+	<?php wp_head(); ?>
+
 	<body class="loading">
 		<svg class="hidden">
 			<symbol id="icon-arrow" viewBox="0 0 24 24">
@@ -101,5 +104,7 @@
 		<script src="js/imagesloaded.pkgd.min.js"></script>
 		<script src="js/TweenMax.min.js"></script>
 		<script src="js/demo.js"></script>
+
+		<?php wp_footer(); ?>
 	</body>
 </html>
